ec3ee2b40026cb51eeedab3fab7468ef8f09f0b9
Shiraz <shiraz.baig@outlook.com>
2026-08-14 12:55:26 +0100

    Developed core understanding

---
+++ main.py
@@ -0,0 +1,13 @@
+import serial
+import time
+
+ser = serial.Serial(port="COM3",baudrate=115200)
+time.sleep(2)
+ser.reset_input_buffer()
+
+while True:
+    if (ser.in_waiting != 0):
+        rawBytes = ser.readline()
+        serialString = rawBytes.decode("utf-8",errors="ignore")
+        serialString.strip()
+        serialStringArray = serialString.split(",")

4d5e1b12b394382a1a6be9f23a3f14dd091c3087
Shiraz <shiraz.baig@outlook.com>
2026-08-17 08:46:30 +0100

    Calibration + Pitch and Roll

--- Learning_MPU6050/Learning_MPU6050.ino
+++ Learning_MPU6050/Learning_MPU6050.ino
@@ -17,20 +17,35 @@ void setup() {
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 }
 
+float calibration(float someAcc,float maxReading, float minReading) { //Arduino Tutorial 79
+  float offsetError = (maxReading + minReading) / 2;
+  float scaleError = 19.62 / (maxReading - minReading);
+  return scaleError * (someAcc - offsetError);
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-  Ax = a.acceleration.x;
+  Ax = a.acceleration.x; 
   Ay = a.acceleration.y;
   Az = a.acceleration.z;
+  float cAx = calibration(Ax,10.2,-9.36);
+  float cAy = calibration(Ay,9.50,-10.07);
+  float cAz = calibration(Az,10.0,-10.0);
+  float pitch = asin(cAy / 9.81) * (180.0 / PI);
+  float roll = asin(cAx / 9.81) * (180.0 / PI);
   AccCurrentTime = millis();
   if (AccCurrentTime - AccPreviousTime > AccDelayTime) {
-    Serial.print(Ax);
+    Serial.print(cAx);
     Serial.print(",");
-    Serial.print(Ay);
+    Serial.print(cAy);
     Serial.print(",");
-    Serial.println(Az);
+    Serial.print(cAz);
+    Serial.print(",");
+    Serial.print(pitch);
+    Serial.print(",");
+    Serial.println(roll);
     AccPreviousTime = AccCurrentTime;
   }
 }

9bbc9206f6bef9bd2ba85b8c36e8dc49d337511d
Shiraz <shiraz.baig@outlook.com>
2026-08-18 16:45:29 +0100

    Gyros added, Pitch, Roll, Yaw calculated.

--- Learning_MPU6050/Learning_MPU6050.ino
+++ Learning_MPU6050/Learning_MPU6050.ino
@@ -4,16 +4,24 @@
 float Ax;
 float Ay;
 float Az;
+float Gx;
+float Gy;
+float Gz;
 float cAx;
 float cAy;
 float cAz;
+float accPitch = 0.0;
+float accRoll = 0.0;
 float pitchRAW;
 float rollRAW;
-float pitch = 0.0;
-float roll = 0.0;
+float gyroPitch = 0.0;
+float gyroRoll = 0.0;
+float gyroYaw = 0.0;
 float confidenceFactor = 0.05;
 unsigned long AccCurrentTime;
 unsigned long AccPreviousTime = 0;
+unsigned long PRYCurrentTime = 0;
+unsigned long PRYPreviousTime = 0;
 int AccDelayTime = 100;
 
 Adafruit_MPU6050 mpu;
@@ -22,6 +30,7 @@ void setup() {
   Serial.begin(115200);
   mpu.begin();
   mpu.setAccelerometerRange(MPU6050_RANGE_2_G); // plus or minus 2g
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 }
 
@@ -59,10 +68,10 @@ void accelerationPrinter(String someLetter) {
 void pitchAndRollPrinter(String someLetter) {
   if (someLetter == "f") {
     Serial.print("Pitch:");
-    Serial.print(pitch);
+    Serial.print(accPitch);
     Serial.print(",");
     Serial.print("Roll:");
-    Serial.print(roll);
+    Serial.print(accRoll);
     Serial.print(",");
   }
   else if (someLetter == "r") {
@@ -75,18 +84,22 @@ void pitchAndRollPrinter(String someLetter) {
   }
 }
 
-void boundaryPrinter() {
+void boundaryPrinter(int upperLimit, int lowerLimit) {
     Serial.print("UL:");
-    Serial.print(90);
+    Serial.print(upperLimit);
     Serial.print(",");
     Serial.print("LL:");
-    Serial.print(-90);
-    Serial.print(",");
-    Serial.println(".");
+    Serial.println(lowerLimit);
 }
 
 void pySerialPrinter() {
   Serial.print("Stuff still needs to be added to this function!!!");
+}
+
+void gyroPitchRollYaw(String rotation, float someG) {
+  if (rotation == "Roll") { //So then Gy must be given. 
+
+  }
 }
 
 void loop() {
@@ -96,19 +109,37 @@ void loop() {
   Ax = a.acceleration.x; 
   Ay = a.acceleration.y;
   Az = a.acceleration.z;
+  Gx = g.gyro.x;
+  Gy = g.gyro.y;
+  Gz = g.gyro.z;
+  PRYCurrentTime = millis();
+  float dt = (PRYCurrentTime - PRYPreviousTime) / 1000.0;
+  PRYPreviousTime = PRYCurrentTime;
+  gyroPitch = gyroPitch + (dt * Gx * (180.0 / PI));
+  gyroRoll += dt * Gy * (180.0 / PI);
+  gyroYaw += dt * Gz * (180.0 / PI);
   cAx = calibration(Ax,10.2,-9.36) / 9.81;
   cAy = calibration(Ay,9.50,-10.07) / 9.81;
   cAz = calibration(Az,10.0,-10.0) / 9.81;
   pitchRAW = atan2(cAy,sqrt((cAz * cAz) + (cAx * cAx))) * (180.0 / PI);
   rollRAW = atan2(cAx,sqrt((cAz * cAz) + (cAy * cAy))) * (180.0 / PI);
-  pitch = ((1.0 - confidenceFactor) * pitch) + (confidenceFactor * pitchRAW);
-  roll = ((1.0 - confidenceFactor) * roll) + (confidenceFactor * rollRAW);
+  accPitch = ((1.0 - confidenceFactor) * accPitch) + (confidenceFactor * pitchRAW);
+  accRoll = ((1.0 - confidenceFactor) * accRoll) + (confidenceFactor * rollRAW);
   AccCurrentTime = millis();
   if (AccCurrentTime - AccPreviousTime > AccDelayTime) {
-    accelerationPrinter("c");
-    pitchAndRollPrinter("f");
-    pitchAndRollPrinter("r");
-    boundaryPrinter();
+    //accelerationPrinter("c");
+    //pitchAndRollPrinter("f");
+    //pitchAndRollPrinter("r");
+    Serial.print("Roll:");
+    Serial.print(gyroRoll);
+    Serial.print(",");
+    Serial.print("Pitch:");
+    Serial.print(gyroPitch);
+    Serial.print(",");
+    Serial.print("Yaw:");
+    Serial.print(gyroYaw);
+    Serial.print(",");
+    boundaryPrinter(180,-180);
     AccPreviousTime = AccCurrentTime;
   }
 }
